07b26b992f881f924853c26695df210d1492b9ff
Chaitanya <chaitanyag1718@gmail.com>
2025-12-12 12:34:05 +0530

    updated 2

--- todo/settings.py
+++ todo/settings.py
@@ -18,7 +18,6 @@ import os
 from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-import dj_database_url
 
 load_dotenv()
 # Quick-start development settings - unsuitable for production
